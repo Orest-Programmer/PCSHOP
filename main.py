71715673678627633d44e13965c58ef8113c401d
Orest-Programmer <orestmusicstudio@gmail.com>
2021-06-10 19:59:37 +0300

    Commit

--- main.py
+++ main.py
@@ -12,13 +12,7 @@ def index():
 def about():
     return render_template(about.html)
 
-@app.route('/about')
-def about():
-    return render_template(about.html)
 
-@app.route('/about')
-def about():
-    return render_template(about.html)
 
 
 if __name__ == "__main__":
